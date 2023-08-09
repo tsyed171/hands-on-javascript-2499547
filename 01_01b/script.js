df7e4672f5c8cbdc582b91647455569cf46258ae
TS <87946132+tsyed171@users.noreply.github.com>
2023-08-09 23:47:28 +0000

    modified and added time component in card

--- 01_01b/script.js
+++ 01_01b/script.js
@@ -4,6 +4,9 @@
  * - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
  * - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleString
  */
+// this gets data from the data.js file
+// created time class to add time component; datetime puts in machine readble time format; add date class to change to american format
+// can add ${imgData.created_at} in datetime category in time class
 
 import data from "./data.js";
 
@@ -11,6 +14,8 @@ const mainContent = document.querySelector(".main-content");
 
 const Card = (data) => {
   const imgData = data[0];
+
+  const date = new Date(imgData.created_at);
 
   const markup = `
     <figure class="image">
@@ -34,6 +39,19 @@ const Card = (data) => {
             Photo by
             <span class="image__photog">${imgData.user.name}</span>.
           </p>
+         
+          <p>
+            Uploaded on
+            <time class="image__date" datetime="">${date.toLocaleString(
+              "default",
+              {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              }
+            )}</time>
+          </p>
+
           <p>
             <a href="${imgData.links.self}" class="image__link">
               View it on Unsplash.
